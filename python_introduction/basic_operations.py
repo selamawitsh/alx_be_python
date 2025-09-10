c569dc5aab8aea5640561754cfa8063832ee4460
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-09-10 15:31:50 +0300

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10 
+number2 = 5 
+
+
+addition_result = number1+number2
+print(
+    f"Addition of {number1} and {number2} is {addition_result}"
+)
+subtraction_result = number1-number2
+print(
+    f"Subtraction of {number1} and {number2} is {subtraction_result}"
+)   
+multiplication_result = number1*number2
+print(
+    f"Multiplication of {number1} and {number2} is {multiplication_result}"
+)
